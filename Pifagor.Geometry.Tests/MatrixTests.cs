ee72a8f1dc8da5672d5960e6e6e56116b9d16808
endAurum <sqreder@gmail.com>
2015-05-20 19:05:11 +0300

    Implement matrix vector equality and addition with tests

--- Pifagor.Geometry.Tests/MatrixTests.cs
+++ Pifagor.Geometry.Tests/MatrixTests.cs
@@ -10,7 +10,19 @@ namespace Pifagor.Geometry.Tests
         {
             var matrixVector = new MatrixVector(1,2);
             Assert.AreEqual(1, matrixVector.X, Compare.AbsTol);
-            Assert.AreEqual(1, matrixVector.Y, Compare.AbsTol);
+            Assert.AreEqual(2, matrixVector.Y, Compare.AbsTol);
+        }
+
+        [Test]
+        public void Addition()
+        {
+            var m1 = new MatrixVector(1,2,3,4);
+            var m2 = new MatrixVector(5,6,7,8);
+            var expected = new MatrixVector(6,8,10,12);
+
+            var actual = m1 + m2;
+
+            Assert.That(actual, Is.EqualTo(expected));
         }
     }
 }
--- Pifagor.Geometry/MatrixVector.cs
+++ Pifagor.Geometry/MatrixVector.cs
@@ -8,23 +8,53 @@ namespace Pifagor.Geometry
 {
     class MatrixVector
     {
-        private readonly double[,] _data = new double[3,3];
+        #region Fields
+
+        private readonly double[,] _data = new double[3, 3];
+
+        #endregion
+
+
+        #region Constructors
 
         public MatrixVector()
         {
-            this[3, 3] = 1;
+            this[2, 2] = 1;
         }
 
-        public MatrixVector(int x, int y)
+        public MatrixVector(double x, double y)
+            : this()
         {
             X = x;
             Y = y;
         }
 
+        public MatrixVector(double x, double y, double dx, double dy)
+            : this()
+        {
+            X = x;
+            Y = y;
+            DX = dx;
+            DY = dy;
+        }
+
+        #endregion
+
+
+        #region Public members
+
         public double this[int row, int col]
         {
             get { return _data[row, col]; }
             set { _data[row, col] = value; }
+        }
+
+        #region Quick accessors
+
+        public double X
+        {
+            get { return _data[0, 0]; }
+            set { _data[0, 0] = value; }
         }
 
         public double Y
@@ -33,11 +63,63 @@ namespace Pifagor.Geometry
             set { _data[1, 1] = value; }
         }
 
-
-        public double X
+        public double DX
         {
-            get { return _data[0, 0]; }
-            set { _data[0, 0] = value; }
+            get { return _data[2, 0]; }
+            set { _data[2, 0] = value; }
+        }
+
+        public double DY
+        {
+            get { return _data[2, 1]; }
+            set { _data[2, 1] = value; }
+        }
+
+
+        #endregion Quick accessors
+
+        #endregion Public members
+
+        #region Operators overloading
+
+        public static MatrixVector operator +(MatrixVector a, MatrixVector b)
+        {
+            return new MatrixVector(a.X + b.X, a.Y + b.Y, a.DX + b.DX, a.DY + b.DY);
+        }
+
+        #endregion
+
+        protected bool Equals(MatrixVector other)
+        {
+            if (!Compare.IsEquals(_data[0, 0], other._data[0, 0])) return false;
+            if (!Compare.IsEquals(_data[1, 1], other._data[1, 1])) return false;
+            if (!Compare.IsEquals(_data[2, 0], other._data[2, 0])) return false;
+            if (!Compare.IsEquals(_data[2, 1], other._data[2, 1])) return false;
+
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((MatrixVector) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return _data?.GetHashCode() ?? 0;
+        }
+
+        public static bool operator ==(MatrixVector left, MatrixVector right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(MatrixVector left, MatrixVector right)
+        {
+            return !Equals(left, right);
         }
     }
 }
