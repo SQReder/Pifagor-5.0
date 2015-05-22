7ff2fd4e6737c4c833fc1b1a9e1957e6fe040f9f
endAurum <sqreder@gmail.com>
2015-05-22 09:04:21 +0300

    Implement get layer number for cluster by number

---
+++ Pifagor.ClusterTreeTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется посредством следующего 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Pifagor.ClusterTreeTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pifagor.ClusterTreeTests")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Задание значения false для атрибута ComVisible приведет к тому, что типы из этой сборки станут невидимыми 
+// для COM-компонентов.  Если к одному из типов этой сборки необходимо обращаться из 
+// модели COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Если данный проект доступен для модели COM, следующий GUID используется в качестве идентификатора библиотеки типов
+[assembly: Guid("c3484745-3d60-4b65-9d1a-72d7a0a47959")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [сборка: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Pifagot.ClusterTree/ClusterMath.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Pifagor.ClusterTree
+{
+    public static class ClusterMath
+    {
+        /// <summary>
+        /// Позволяет получить глубину, на которой находится кластер, при заданном порядке дерева
+        /// </summary>
+        /// <param name="index">Номер кластера</param>
+        /// <param name="treeBase">Порядок дерева</param>
+        /// <returns>Номер уровня, на котором находится нода</returns>
+        public static int GetLayerNumber(int index, int treeBase)
+        {
+            if (index < 0)
+                throw new ArgumentOutOfRangeException(nameof(index), index, "Index must be greater or equal zero");
+            if (treeBase < 1)
+                throw new ArgumentOutOfRangeException(nameof(treeBase), treeBase, "Index must be greater than 0");
+            if (index == 0)
+                return 0;
+
+            var n = 1;
+            do
+            {
+                index -= (int)Math.Floor(Math.Pow(treeBase, n));
+                n++;
+            }
+            while (index > 0);
+            return n-1;
+        }
+    }
+}
